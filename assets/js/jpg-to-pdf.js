ac75a9d14dc37ca0d24222ed7b76ae37c9a7b177
watersalf27-arch <watersalf27@gmail.com>
2026-06-29 00:57:09 -0700

    Update jpg-to-pdf.js

--- assets/js/jpg-to-pdf.js
+++ assets/js/jpg-to-pdf.js
@@ -1,1 +1,44 @@
-7
+const input = document.getElementById("images");
+const preview = document.getElementById("preview");
+const convertBtn = document.getElementById("convertBtn");
+const status = document.getElementById("status");
+
+let selectedImages = [];
+
+input.addEventListener("change", () => {
+
+preview.innerHTML = "";
+selectedImages = [];
+
+const files = Array.from(input.files);
+
+files.forEach(file => {
+
+if (!file.type.startsWith("image/")) return;
+
+selectedImages.push(file);
+
+const reader = new FileReader();
+
+reader.onload = function(e){
+
+const card = document.createElement("div");
+card.className = "preview-card";
+
+card.innerHTML = `
+<img src="${e.target.result}" alt="">
+<p>${file.name}</p>
+`;
+
+preview.appendChild(card);
+
+};
+
+reader.readAsDataURL(file);
+
+});
+
+status.textContent =
+selectedImages.length + " image(s) selected.";
+
+});
